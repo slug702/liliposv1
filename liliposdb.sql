eeee0998efdd0129d32f16b22a5bd17b5595fde2
slug702 <almonteinvestorsrelations@gmail.com>
2025-09-18 22:04:06 +0800

    new db

--- liliposdb.sql
+++ liliposdb.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: localhost    Database: lilipos
 -- ------------------------------------------------------
--- Server version	8.3.0
+-- Server version	8.0.39
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,31 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `categories`
---
-
-DROP TABLE IF EXISTS `categories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `categories` (
-  `category_id` int NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(45) DEFAULT NULL,
-  `category_type` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `categories`
---
-
-LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'All','All'),(2,'Appetizers','appetizers'),(3,'Sides','sides'),(4,'Ice Cream','dessert'),(5,'Cookies','baked goods'),(6,'Chicken','meat'),(7,'Pork','meat'),(8,'Beef','meat'),(9,'Vegetables','vegan'),(10,'Puppies','pupcups'),(11,'Mains','Main');
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `invoices`
 --
 
@@ -48,19 +23,14 @@ DROP TABLE IF EXISTS `invoices`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `invoices` (
-  `iid` int NOT NULL AUTO_INCREMENT,
-  `idate` date DEFAULT NULL,
-  `iproducts` varchar(45) DEFAULT NULL,
-  `mop` varchar(45) DEFAULT NULL,
-  `tprce` float DEFAULT NULL,
-  `pricebeforedscnt` float DEFAULT NULL,
-  `idscnt` float DEFAULT NULL,
-  `vat` float DEFAULT NULL,
-  `paid` float DEFAULT NULL,
-  `change` float DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
+  `inv_id` int NOT NULL AUTO_INCREMENT,
+  `table_num` int DEFAULT NULL,
   `cashier` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`iid`)
+  `order_type` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `total_vat` float DEFAULT NULL,
+  `order_date` date DEFAULT NULL,
+  PRIMARY KEY (`inv_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -89,6 +59,7 @@ CREATE TABLE `products` (
   `product_picture` longblob,
   `size` varchar(45) DEFAULT NULL,
   `size_group` varchar(45) DEFAULT NULL,
+  `vat` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`pid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -99,7 +70,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'chickenjoy',200,'Mains','Chicken',NULL,'No Size','No Size'),(3,'cheeseburger',120,'Mains','Beef',NULL,'No Size','No Size'),(4,'chickenskin',100,'Sides','Chicken',NULL,'No Size','No Size'),(5,'humba',300,'Mains','Pork',NULL,'No Size','No Size'),(6,'noodles',3200,'Appetizers','noodles',NULL,'No Size','No Size'),(7,'cheesy fries',400,'Sides','A',NULL,'Small','No Size'),(9,'Biscuit',100,'Sides','A',NULL,'No Size','No Size'),(12,'Banana Cake',200,'Appetizers','Yes',NULL,'No Size','No Size'),(13,'shawarma fries',150,'Mains','Sides',NULL,'No Size','No Size'),(17,'Birria Quesedilla',250,'Mains','Mexican',NULL,'No Size','No Size'),(18,'crossiant',260,'Sides','Sides',NULL,'No Size','No Size'),(21,'L coffee',250,'Drinks','Main',NULL,'L','coffee'),(24,'Jolli Hotdog',250.2,'Hotdog','Franchies',NULL,'No Size','No Size'),(27,'Frites',250,'French','Potatoes',NULL,'No Size','No Size');
+INSERT INTO `products` VALUES (1,'chickenjoy',200,'Mains','Chicken',NULL,'No Size','No Size',NULL),(3,'cheeseburger',120,'Mains','Beef',NULL,'No Size','No Size',NULL),(4,'chickenskin',100,'Sides','Chicken',NULL,'No Size','No Size',NULL),(5,'humba',300,'Mains','Pork',NULL,'No Size','No Size',NULL),(6,'noodles',3200,'Appetizers','noodles',NULL,'No Size','No Size',NULL),(7,'cheesy fries',400,'Sides','A',NULL,'Small','No Size',NULL),(9,'Biscuit',100,'Sides','A',NULL,'No Size','No Size',NULL),(12,'Banana Cake',200,'Appetizers','Yes',NULL,'No Size','No Size',NULL),(13,'shawarma fries',150,'Mains','Sides',NULL,'No Size','No Size',NULL),(17,'Birria Quesedilla',250,'Mains','Mexican',NULL,'No Size','No Size',NULL),(18,'crossiant',260,'Sides','Sides',NULL,'No Size','No Size',NULL),(21,'L coffee',250,'Drinks','Main',NULL,'L','coffee',NULL),(24,'Jolli Hotdog',250.2,'Hotdog','Franchies',NULL,'No Size','No Size',NULL),(27,'Frites',250,'French','Potatoes',NULL,'No Size','No Size',NULL);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -111,13 +82,19 @@ DROP TABLE IF EXISTS `transactions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `transactions` (
-  `tid` int NOT NULL AUTO_INCREMENT,
-  `trpid` int DEFAULT NULL,
-  `trdesc` varchar(45) DEFAULT NULL,
-  `trprce` float DEFAULT NULL,
-  `dscnt` float DEFAULT NULL,
-  `invid` int DEFAULT NULL,
-  PRIMARY KEY (`tid`)
+  `tr_id` int NOT NULL AUTO_INCREMENT,
+  `inv_id` int DEFAULT NULL,
+  `p_id` int DEFAULT NULL,
+  `tr_type` varchar(45) DEFAULT NULL,
+  `tr_desc` varchar(45) DEFAULT NULL,
+  `tr_price` float DEFAULT NULL,
+  `debit` float DEFAULT NULL,
+  `credit` float DEFAULT NULL,
+  `vat` varchar(45) DEFAULT NULL,
+  `tr_dscnt` float DEFAULT NULL,
+  `mop` varchar(45) DEFAULT NULL,
+  `tr_date` date DEFAULT NULL,
+  PRIMARY KEY (`tr_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -167,4 +144,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-18 21:31:01
+-- Dump completed on 2025-09-18 22:03:53
